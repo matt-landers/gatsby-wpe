025e3f3792c438653ea292044d6dfe1f571827b7
Matt Landers <matt@platypi.io>
2020-04-16 09:34:34 -0500

    initial project

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.linkedin.com/in/mattlanders24/">Matt Landers</a>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,69 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class Homepage extends Component {
+  render() {
+    const data = this.props.data
 
-export default IndexPage
+    return (
+      <Layout>
+        <SEO title="Gatsby on WP Engine" />
+        {/* <div>
+          <h1>Pages</h1>
+          {data.allWordpressPage.edges.map(({ node }) => (
+            <div key={node.slug}>
+              <Link to={node.slug}>
+                <h2>{node.title}</h2>
+              </Link>
+              <h3 dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          ))}
+        </div> */}
+
+        {/* <h1>Posts</h1> */}
+        {data.allWordpressPost.edges.map(({ node }) => (
+          <div key={node.slug}>
+            <Link
+              to={node.slug}
+              style={{
+                color: `rebeccapurple`,
+                textDecoration: `none`,
+              }}
+            >
+              <h2>{node.title}</h2>
+            </Link>
+            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        ))}
+      </Layout>
+    )
+  }
+}
+
+export default Homepage
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+    allWordpressPost {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
