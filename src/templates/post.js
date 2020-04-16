3f55ad899e8df1f25f8d2f39d074d01893316a4b
Matt Landers <matt@platypi.io>
2020-04-16 09:41:33 -0500

    updated readme

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,12 +2,15 @@ import React, { Component } from "react"
 import { graphql } from "gatsby"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 class Post extends Component {
   render() {
     const post = this.props.data.wordpressPost
 
     return (
       <Layout>
+        <SEO title={post.title} />
         <h1>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </Layout>
